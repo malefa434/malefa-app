b8d585cdf03c408ac1050ec72244868382c99165
malefa434 <malefanuralmaira@gmail.com>
2025-05-21 14:52:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,4 @@ import streamlit as st
 
 st.title("🎈 welcome guys")
 st.write(
-    "Let's start building!"
-)
-st.image("https://github.com/malefa434/malefa-app/blob/main/IMG-20250301-WA0103.jpg",width=200)
+st.image("IMG-20250301-WA0103.jpg",width=200)
